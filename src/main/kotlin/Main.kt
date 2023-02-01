f4815a39dcee5bf15b96c3c2554578a2c7298e39
Jim TJ <jimtore2k@gmail.com>
2023-02-02 00:48:51 +0100

    Oh shit, this is the messiest shit ever

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,19 @@
+import java.lang.IndexOutOfBoundsException
+
 fun main() {
     print("Type in the first players name: ")
     val player1 = Player(readln(), 1)
     print("Type in the second players name: ")
     val player2 = Player(readln(),2)
+    clearScreen()
     val game = TicTacToe(player1, player2)
-
-
-
     game.startGame()
+}
+
+fun clearScreen(){
+    for (i in 1..50){ // Very unnecessary but there is no system.os('cls') in Kotlin afaik...
+        print("\n")
+    }
 }
 
 abstract class Game {
@@ -24,6 +30,7 @@ class Player(var name: String, val playerNumber: Int, var score: Int = 0)
 
 class TicTacToe(private var player1: Player, private var player2: Player): Game(),GameBoard {
     init{
+        clearScreen()
         println("How to play:")
         println("You use 1-9 to select your square, like the pattern below:")
         println(" 1   2   3")
@@ -32,16 +39,31 @@ class TicTacToe(private var player1: Player, private var player2: Player): Game(
         println("-Let's go-")
         println("----------")
     }
-        override val type = "Simple Board Game"
-        override val numberOfPlayers = 2
-        var ticTacToeBoard = mutableListOf<MutableList<String>>()
+    private var player1Mark = "X"
+    private var player2Mark = "O"
+    private var activePlayer = player1
+    private var activePlayerMark = player1Mark
 
-    //Lambda expression to check if a value is a number or not, returns bool
-    val isANumber = { toCheck: String -> toCheck.toDoubleOrNull() != null }
+    override val type = "Simple Board Game"
+    override val numberOfPlayers = 2
+    private var ticTacToeBoard = mutableListOf<MutableList<String>>()
+
+    //Lambda expression to check if a value is a number between 0 and 9 or not, returns bool
+    private val isANumber = { toCheck: String -> toCheck.toDoubleOrNull() != null }
+
+    private fun validateInput(input: String, isANumber: (String) -> Boolean): Boolean {
+        val number: Double? = input.toDoubleOrNull()
+        return !isANumber(input) || number == null || number.toInt().toDouble() != number || number !in 1.0..9.0
+    }
 
     override fun isValidMove(row: Int, column: Int, player: Int): Boolean{
+        try{
         val checkedCell = ticTacToeBoard[row][column]
         return checkedCell == "-"
+        }
+        catch (e: IndexOutOfBoundsException){
+            return false
+        }
     }
 
     override fun createBoard(boardSize: Int){
@@ -78,7 +100,7 @@ class TicTacToe(private var player1: Player, private var player2: Player): Game(
         return listOf("-1","-")
     }
 
-    //Method to print the game board
+    //Prints the game board
     private fun printGameBoard(){
         println("Tic Tac Toe")
         for (i in 0..2){
@@ -86,20 +108,18 @@ class TicTacToe(private var player1: Player, private var player2: Player): Game(
         }
     }
 
-    //Method to check who wins
-    private fun checkWhoWins(winList: List<String>, player1Name: Player, player2Name: Player, typeOfWin: String){
-        val player1Mark = "X"
-        val player2Mark = "O"
+    //Check who wins
+    private fun checkWinner(winList: List<String>, typeOfWin: String){
         if (winList[1] == player1Mark){
-            println("Congratulations, ${player1Name.name}, you won with ${winList[1]} on $typeOfWin ${winList[0].toInt()}")
-            player1Name.score++
+            println("Congratulations, ${player1.name}, you won with ${winList[1]} on $typeOfWin ${winList[0].toInt()}")
+            player1.score++
             println("The score is now:")
-            println("${player1Name.name} ${player1Name.score} - ${player2Name.score} ${player2Name.name}")
+            println("${player1.name} ${player1.score} - ${player2.score} ${player2.name}")
         } else if(winList[1] == player2Mark){
-            println("Congratulations, ${player2Name.name}, you won with ${winList[1]} on $typeOfWin ${winList[0].toInt()}")
-            player2Name.score++
+            println("Congratulations, ${player2.name}, you won with ${winList[1]} on $typeOfWin ${winList[0].toInt()}")
+            player2.score++
             println("The score is now:")
-            println("${player1Name.name} ${player1Name.score} - ${player2Name.score} ${player2Name.name}")
+            println("${player1.name} ${player1.score} - ${player2.score} ${player2.name}")
         }
     }
 
@@ -116,10 +136,26 @@ class TicTacToe(private var player1: Player, private var player2: Player): Game(
             }
         }
     }
+    private fun resetGameBoard(){
+        clearScreen()
+        println("Swapping who plays first")
+        println("${player2.name}, you are now playing first!")
+        println("")
+        ticTacToeBoard = mutableListOf()
+        createBoard(3)
+        val tempMark = player1Mark
+        player1Mark = player2Mark
+        player2Mark = tempMark
+        val tempPlayer = player1
+        player1 = player2
+        player2 = tempPlayer
+        activePlayer = player1
+        activePlayerMark = player1Mark
+    }
 
     //Turns string input into int (as this has already been checked to be valid input in while loops). Returns index of outer and inner lists in ticTacToeBoard.
     private fun cleanPlayerInput(input: String): List<Int>{
-        val cleanInput = input.toInt()
+        val cleanInput = input.toDouble().toInt()
         val outerIndex = (cleanInput-1) / 3
         val innerIndex = (cleanInput-1) % 3
         return mutableListOf(outerIndex,innerIndex)
@@ -142,98 +178,104 @@ class TicTacToe(private var player1: Player, private var player2: Player): Game(
         return validMovesList
     }
 
+    private fun printValidMoves(currentValidMoves: List<Int>){
+        clearScreen()
+        printGameBoard()
+        println("Valid moves: ")
+        for (i in currentValidMoves){
+            print("$i ")
+        }
+        println("")
+    }
+
     //Game loop
     fun startGame(){
         createBoard(3)
-        val player1Mark = "X"
-        val player2Mark = "O"
-        var activePlayer = player1
-        var activePlayerMark = player1Mark
-
+        clearScreen()
         while(true){
             printGameBoard()
             val rows = checkRows()
-            if (rows[0].toInt() != -1){ //If there is a winner on rows, check who it is
-                checkWhoWins(rows,player1,player2,"row")
-                if (restartGame() == "y"){
-                    ticTacToeBoard = mutableListOf()
-                    createBoard(3)
-                    activePlayer = player1
-                    activePlayerMark = player1Mark
-                } else {
-                    break
-                }
-            }
-
             val columns = checkColumns()
-            if (columns[0].toInt() != -1){ //If there is a winner on columns, check who it is
-                checkWhoWins(columns,player1,player2,"column")
-                if (restartGame() == "y"){
-                    ticTacToeBoard = mutableListOf()
-                    createBoard(3)
-                    activePlayer = player1
-                    activePlayerMark = player1Mark
-                } else {
-                    break
-                }
-            }
-
             val diagonals = checkDiagonals()
-            if (diagonals[0].toInt() != -1){ //If there is a winner on diagonals, check who it is
-                checkWhoWins(diagonals,player1,player2,"diagonal")
-                if (restartGame() == "y"){
-                    ticTacToeBoard = mutableListOf()
-                    createBoard(3)
-                    activePlayer = player1
-                    activePlayerMark = player1Mark
-                } else {
-                    break
-                }
-            }
             val currentValidMoves = validMoves()
-            if (currentValidMoves.isEmpty()){
-                println("The game is a tie!")
-                if (restartGame() == "y"){
-                    ticTacToeBoard = mutableListOf()
-                    createBoard(3)
-                    activePlayer = player1
-                    activePlayerMark = player1Mark
-                } else {
-                    break
-                }
-            }
-            println("Valid moves: ")
             for (i in currentValidMoves){
                 print("$i ")
             }
+
             println("")
+
+            if (rows[0].toInt() != -1){ //If there is a winner on rows, check who it is
+                checkWinner(rows,"row")
+                if (restartGame() == "y"){
+                    resetGameBoard()
+                    continue
+                } else {
+                    break
+                }
+            }
+            if (columns[0].toInt() != -1){ //If there is a winner on columns, check who it is
+                checkWinner(columns,"column")
+                if (restartGame() == "y"){
+                    resetGameBoard()
+                    continue
+                } else {
+                    break
+                }
+            }
+            if (diagonals[0].toInt() != -1){ //If there is a winner on diagonals, check who it is
+                checkWinner(diagonals,"diagonal")
+                if (restartGame() == "y"){
+                    resetGameBoard()
+                    continue
+                } else {
+                    break
+                }
+            }
+            if (currentValidMoves.isEmpty()){
+                println("The game is a tie!")
+                println("The score is now:")
+                println("${player1.name} ${player1.score} - ${player2.score} ${player2.name}")
+                if (restartGame() == "y"){
+                    resetGameBoard()
+                    continue
+                } else {
+                    break
+                }
+            }
+
             println("${activePlayer.name}, you are ${activePlayerMark}, enter a valid move:")
             var playerInput = readln()
-            while (playerInput == "0" || !isANumber(playerInput)){
+
+            while (validateInput(playerInput, isANumber)){
+                printValidMoves(currentValidMoves)
                 println("Not a valid move! ${activePlayer.name}, you are ${activePlayerMark}, enter a valid move:")
                 playerInput = readln()
             }
             var listIndices = cleanPlayerInput(playerInput)
             var isValid = isValidMove(listIndices[0],listIndices[1],activePlayer.playerNumber)
+
             while (!isValid){
-                println("${activePlayer.name}, you are ${activePlayerMark}, enter a valid move:")
+                printValidMoves(currentValidMoves)
+                println("Not a valid move! ${activePlayer.name}, you are ${activePlayerMark}, enter a valid move:")
                 playerInput = readln()
-                listIndices = cleanPlayerInput(playerInput)
-                isValid = isValidMove(listIndices[0],listIndices[1],activePlayer.playerNumber)
-                while (playerInput == "0" || !isANumber(playerInput)){
+                while (validateInput(playerInput, isANumber)){
+                    printValidMoves(currentValidMoves)
                     println("Not a valid move! ${activePlayer.name}, you are ${activePlayerMark}, enter a valid move:")
                     playerInput = readln()
                 }
+                listIndices = cleanPlayerInput(playerInput)
+                isValid = isValidMove(listIndices[0],listIndices[1],activePlayer.playerNumber)
             }
+            clearScreen()
             if (activePlayer == player1){
                 ticTacToeBoard[listIndices[0]][listIndices[1]] = player1Mark
                 activePlayerMark = player2Mark
                 activePlayer = player2
                 continue
             }
-            ticTacToeBoard[listIndices[0]][listIndices[1]] = player2Mark
-            activePlayerMark = player1Mark
-            activePlayer = player1
+                ticTacToeBoard[listIndices[0]][listIndices[1]] = player2Mark
+                activePlayerMark = player1Mark
+                activePlayer = player1
         }
     }
 }
